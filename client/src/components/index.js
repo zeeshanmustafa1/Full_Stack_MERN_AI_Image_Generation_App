382da8a6043f401d3f28b5948de760b7777e378f
zeeshan <zeeshan.mustafa@emerald-labs.com>
2023-06-22 15:54:55 +0500

    home page completed and cards component made

---
+++ client/src/components/index.js
@@ -0,0 +1,9 @@
+import Card from "./Card.jsx";
+import FormFields from "./FormFields.jsx";
+import Loader from "./Loader.jsx";
+
+export {
+  Card,
+  FormFields,
+  Loader
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, {useState} from "react";
+import {Card, FormFields, Loader} from "../components/index.js";
+
+const RenderCards = ({data, title}) => {
+  if (data.length > 0) {
+    return data.map(
+      (post) => <Card key={post._id} {...post} />
+    )
+  }
+
+  return (
+    <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">
+      {title}
+    </h2>
+  )
+}
 
 const Home = () => {
-  return <div>Home</div>;
+  const [loading, setloading] = useState(false);
+  const [allPost, setAllPost] = useState(null)
+  const [searchText, setSearchText] = useState('')
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          The Community Showcase
+        </h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">
+          Browse though a collection of imaginative and visually stunning images by AI
+        </p>
+      </div>
+
+      <div className="mt-16">
+        <FormFields/>
+      </div>
+
+      <div className="mt-10">
+        {loading ? (
+          <div className="flex justify-cneter items-center">
+            <Loader/>
+          </div>
+        ) : (
+          <>
+            {setSearchText && (
+              <h2 className="font-medium text-[#666e75] text-xl mb-3">
+                Showing result for
+                <span className="text-[#222328]">{searchText}</span>
+              </h2>
+            )}
+            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+              {searchText ?(
+                <RenderCards
+                  data={[]}
+                  title="No result found!!!"
+                />
+              ):(
+                <RenderCards
+                  data={[]}
+                  title="No post found!!!"
+                />
+              )}
+            </div>
+          </>
+        )}
+      </div>
+
+    </section>
+  )
 };
 
 export default Home;
